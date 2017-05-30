751276b727c77a3f3fcc9c9bd3b2972296bdb419
Andrew Stanton <astanton@etsy.com>
2017-05-29 22:04:33 -0700

    version bump

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ extensions = [
 ]
 
 setup(name='fastxml',
-      version="1.0.0",
+      version="1.0.1",
       description='FastXML Extreme Multi-label Classification Algorithm',
       url="https://github.com/refefer/fastxml",
       author_email="refefer@gmail.com",
